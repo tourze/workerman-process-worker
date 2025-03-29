666c3f394b83c951d66ba7a405db38bc60094992
tourze-ci <you@example.com>
2025-03-29 10:40:35 +0000

    chore: 将 enum 目录下的模块统一移动到 packages 目录

---
+++ src/ProcessWorker.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tourze\Workerman\ProcessWorker;
+
+use Workerman\Worker;
+
+class ProcessWorker extends Worker
+{
+    public mixed $onProcessStart = null;
+    public mixed $onProcessOutput = null;
+    public mixed $onProcessExit = null;
+
+    public function __construct(private readonly string $runCommand)
+    {
+        parent::__construct();
+        $this->reloadable = true;
+        $this->count = 1;
+        $this->onWorkerStart = $this->onWorkerStart(...);
+    }
+
+    private function onWorkerStart(): void
+    {
+        $this->onProcessStart && call_user_func_array($this->onProcessStart, [$this]);
+
+        // 使用popen打开一个进程
+        $pipe = popen($this->runCommand, 'r'); // 替换为你的shell命令
+        // 创建读时间
+        Worker::$globalEvent->onReadable($pipe, function($pipe) {
+            // 读取进程输出
+            $output = fread($pipe, 8192);
+            if ($output === false || feof($pipe)) {
+                // 关闭连接和进程
+                fclose($pipe);
+                Worker::$globalEvent->offReadable($pipe);
+                $this->onProcessExit && call_user_func_array($this->onProcessExit, [$this]);
+            } else {
+                // 输出发送给客户端
+                //echo '收到：' . $output;
+                $this->onProcessOutput && call_user_func_array($this->onProcessOutput, [$this, $output]);
+            }
+        });
+    }
+}
